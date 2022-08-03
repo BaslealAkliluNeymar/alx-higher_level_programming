8ef352c1b08323d36df916c06376a2540ddb81ee
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2022-08-03 19:15:07 +0000

    fina

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""6-from_json_string.
+Returns an object
+represented by a JSON string.
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Return the object represented my my_str.
+    Args:
+        - my_str: JSON string representation
+    Returns: corresponding object
+    """
+
+    return json.loads(my_str)
